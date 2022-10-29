561aea78d15fd2fae6db4f931adad2b91575a495
kimizuka <f.kimizuka@gmail.com>
2022-10-29 12:23:47 +0900

    :)

---
+++ main.js
@@ -0,0 +1,19 @@
+(() => {
+  if (/https:\/\/calendar.google.com\/calendar/.test(location.href)) {
+    [].slice.call(document.querySelectorAll('[role="button"] [aria-hidden="true"] > :first-child')).forEach((elm) => { elm.innerHTML = '予定あり' });
+    html2canvas(document.querySelector('[data-view-heading]')).then((canvas) => {
+      downloadImage(canvas.toDataURL());
+    });
+    
+    function downloadImage(dataUrl) {
+      const name = 'calendar.png';
+      const a = document.createElement('a');
+
+      a.href = dataUrl;
+      a.download = name;
+      a.click();
+    }
+  } else {
+    window.open('https://calendar.google.com/calendar/');
+  }
+})();
