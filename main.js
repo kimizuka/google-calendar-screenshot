65132da3bded83afef0d517ffea90a5441731547
kimizuka <f.kimizuka@gmail.com>
2022-12-28 10:30:52 +0900

    remove line

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 (() => {
   if (/https:\/\/calendar.google.com\/calendar/.test(location.href)) {
     [].slice.call(document.querySelectorAll('[role="button"] [aria-hidden="true"] > :first-child')).forEach((elm) => { elm.innerHTML = '予定あり' });
+    [].slice.call(document.querySelectorAll('[aria-hidden="true"][style^="top"]')).forEach((elm) => { elm.style.display = 'none' });
     html2canvas(document.querySelector('[data-view-heading]')).then((canvas) => {
       downloadImage(canvas.toDataURL());
     });
-    
+
     function downloadImage(dataUrl) {
       const name = 'calendar.png';
       const a = document.createElement('a');
